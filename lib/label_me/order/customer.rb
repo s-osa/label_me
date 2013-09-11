601134dc0d8749fd87d6f747885206b3b03a70ef
OSA Shunsuke <hhelibebcnofnenamg@gmail.com>
2013-09-11 17:17:07 +0900

    LabelMe::Order::Customer

--- lib/label_me/order/customer.rb
+++ lib/label_me/order/customer.rb
@@ -2,7 +2,20 @@ module LabelMe
   class Order
     class Customer
       def initialize(row)
+        @number       = row.customer_id.to_i
+        @name         = row.customer_name
+        @email        = row.customer_email
+        @phone_number = row.customer_phone_number
+        @address      = row.customer_address
+        @postal_code  = row.customer_postal_code
       end
+
+      attr_accessor :number
+      attr_accessor :name
+      attr_accessor :email
+      attr_accessor :phone_number
+      attr_accessor :address
+      attr_accessor :postal_code
     end
   end
 end
---
+++ spec/label_me/order/customer_spec.rb
@@ -0,0 +1,48 @@
+#coding:utf-8
+require "spec_helper"
+
+describe LabelMe::Order::Customer do
+  before do
+    order = LabelMe::SalesAll::Object.parse("spec/csv/sales_all.csv").first
+    @customer = order.customer
+  end
+
+  describe "attributes" do
+    describe "#number" do
+      it "should return customer number" do
+        expect(@customer.number).to eq(23453456)
+      end
+    end
+
+    describe "#name" do
+      it "should return customer name" do
+        expect(@customer.name).to eq("桃瀬くるみ")
+      end
+    end
+
+    describe "#email" do
+      it "should return customer email" do
+        expect(@customer.email).to eq("kurumi@example.com")
+      end
+    end
+
+    describe "#phone_number" do
+      it "should return customer phone number" do
+        expect(@customer.phone_number).to eq("098-765-4321")
+      end
+    end
+
+    describe "#address" do
+      it "should return customer address" do
+        expect(@customer.address).to match(/東京都大田区足柄.+402/)
+      end
+    end
+
+    describe "#postal_code" do
+      it "should return customer postal code" do
+        expect(@customer.postal_code).to eq("1000001")
+      end
+    end
+  end
+end
+
